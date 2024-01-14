b8a694bd818c30f885a1c3c88d9a3b155dbfc83f
doggysir <78118570+doggysir@users.noreply.github.com>
2024-01-14 11:57:26 -0500

    Returns Angle and Distance (change the values of mountAngle, limelightHeight and goalHeight).

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -5,10 +5,26 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
+    NetworkTable tableInstance;
+    double ty;
+    double mountAngle = 25.0;
+    double limelightHeight = 20.0;
+    double goalHeight = 60.0;
     public VisionSubsystem() {}
+
+    public double getDistance(){
+        ty = tableInstance.getEntry("ty").getDouble(0.0);
+        double degrees = mountAngle + ty;
+        double radians = degrees * (3.14159 / 180.0);
+        double distance = (goalHeight - limelightHeight) / Math.tan(radians);
+        return distance;
+    }
+    public double getAngleDifferential(){
+        return ty;
+    }
     @Override
     public void periodic() {
-        NetworkTable tableInstance = NetworkTableInstance.getDefault().getTable("limelight");
+        tableInstance = NetworkTableInstance.getDefault().getTable("limelight");
     }
 
 
