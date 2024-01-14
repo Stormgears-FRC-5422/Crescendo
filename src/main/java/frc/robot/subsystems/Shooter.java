33ed371115d32869388a7409b27eee0bad8c43b5
mpillai2 <meher.pillai@gmail.com>
2024-01-14 12:07:18 -0500

    made shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Shooter extends SubsystemBase {
+}
+
+
+
