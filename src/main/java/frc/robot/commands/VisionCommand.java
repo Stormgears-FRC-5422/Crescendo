a3b423393bec334ebde07c1b3f1335455d66eb79
doggysir <78118570+doggysir@users.noreply.github.com>
2024-01-14 20:50:42 -0500

    implemented getDistance() into new VisionCommand
    Signed-off-by: doggysir <78118570+doggysir@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/VisionCommand.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.VisionSubsystem;
+
+public class VisionCommand extends Command {
+    VisionSubsystem visionSubsystem;
+    double distanceFromTarget;
+
+    VisionCommand(VisionSubsystem visionSubsystem) {
+        this.visionSubsystem = visionSubsystem;
+        addRequirements(visionSubsystem);
+
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    public void execute() {
+        distanceFromTarget = visionSubsystem.getDistance();
+        SmartDashboard.putNumber("Distance From Target", distanceFromTarget);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+}
--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -6,7 +6,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
     NetworkTable tableInstance;
+
     double ty;
+
     double tx;
     double MOUNT_ANGLE_DEGREES = 25.0;
     double LIMELIGHT_HEIGHT = 20.0;
@@ -23,8 +25,19 @@ public class VisionSubsystem extends SubsystemBase {
         return ((GOAL_HEIGHT - LIMELIGHT_HEIGHT) / Math.tan(radians)) / Math.cos(tx);
     }
 
+    public double getVerticalAngle() {
+        return ty;
+    }
+
+    public double getHorizontalAngle() {
+        return tx;
+    }
+
     @Override
     public void periodic() {
+        double[] pose = tableInstance.getEntry("camerapose_targetspace").getDoubleArray(new double[6]);
+
+
         tableInstance = NetworkTableInstance.getDefault().getTable("limelight");
     }
 }
