d119fff4c71ab6b3281ba18fd62a3c7007513d2f
deepak <deepak.parmar@gmail.com>
2024-01-15 18:49:50 -0500

    Advantage Kit initial setup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,69 +4,67 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.Constants.Toggles;
-import frc.robot.Constants.Drive;
+import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.JoyStickDrive;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.subsystems.NavX;
-import frc.robot.subsystems.drive.DrivetrainBase;
-import frc.robot.subsystems.drive.DrivetrainFactory;
-import frc.robot.subsystems.drive.IllegalDriveTypeException;
-import frc.utils.joysticks.StormXboxController;
 
-
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
 public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  NavX navX;
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
-  DrivetrainBase drivetrainBase;
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    System.out.println("[Init] RobotContainer");
 
-  StormXboxController xboxController;
-
-  public RobotContainer() throws IllegalDriveTypeException {
-    if (Toggles.useDrive) {
-
-      drivetrainBase = DrivetrainFactory.getInstance(Drive.driveType);
-
-      if (Toggles.useController){
-
-        xboxController = new StormXboxController(0);
-        JoyStickDrive driveWithJoystick = new JoyStickDrive(
-                drivetrainBase,
-                xboxController::getWpiXSpeed,
-                xboxController::getWpiYSpeed,
-                xboxController::getOmegaSpeed,
-                () -> xboxController.getLeftTrigger() > 0.2,
-                () -> xboxController.getRightTrigger() > 0.2
-        );
-        drivetrainBase.setDefaultCommand(driveWithJoystick);
-      }
-    }
-
-    if (Toggles.useNavX) {
-      navX = new NavX();
-    }
-
-
-
-
-
-
+    // Configure the trigger bindings
     configureBindings();
+
+    System.out.println("[DONE] RobotContainer");
   }
 
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
   private void configureBindings() {
+    System.out.println("[Init] configureBindings");
 
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    System.out.println("[DONE] configureBindings");
   }
 
-
-//  public Command getAutonomousCommand() {
-//
-//  }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
 }
