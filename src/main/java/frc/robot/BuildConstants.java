5ff7472ef066221cb4daa61256be27bf4cdf7e4a
darrenleomiller <darrenleomiller@gmail.com>
2024-01-16 19:26:09 -0500

    A bit more testing. Still need to set encoder offset

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 23;
-  public static final String GIT_SHA = "aa321cb01a3ad50b0763a91baf5c5c22e905a2a5";
-  public static final String GIT_DATE = "2024-01-16 00:17:15 EST";
+  public static final int GIT_REVISION = 26;
+  public static final String GIT_SHA = "24a467329a244d1622401a87b5c0324aced7b5af";
+  public static final String GIT_DATE = "2024-01-16 00:24:52 EST";
   public static final String GIT_BRANCH = "drive";
-  public static final String BUILD_DATE = "2024-01-16 00:22:17 EST";
-  public static final long BUILD_UNIX_TIME = 1705382537379L;
+  public static final String BUILD_DATE = "2024-01-16 19:09:08 EST";
+  public static final long BUILD_UNIX_TIME = 1705450148987L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
