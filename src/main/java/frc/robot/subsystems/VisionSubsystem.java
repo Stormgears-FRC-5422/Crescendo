303ee3a5f8e07fdbadb342bf8a3596f5493df441
doggysir <78118570+doggysir@users.noreply.github.com>
2024-02-03 13:58:02 -0500

    Coded all functions
    Signed-off-by: doggysir <78118570+doggysir@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,10 +1,23 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
+    private boolean initialized = false;
+    private NetworkTableEntry tTarget = null;
+    private NetworkTableEntry txEntry = null;
+    private NetworkTableEntry tyEntry = null;
+    private NetworkTableEntry taEntry = null;
+    private NetworkTableEntry botpose = null;
+    private NetworkTableEntry targetpose = null;
+    private NetworkTableEntry tl = null;
+    private NetworkTableEntry cl = null;
+    private Alliance alliance = Alliance.Blue;
     NetworkTable tableInstance;
 
     double ty;
@@ -33,11 +46,95 @@ public class VisionSubsystem extends SubsystemBase {
         return tx;
     }
 
+    public boolean isInitialized() {
+        return this.initialized;
+    }
+
+    public boolean hasTargets() {
+        boolean hits = false;
+        //SmartDashboard.putBoolean("isInitialized", isInitialized());
+        if (isInitialized()) {
+            hits = (tableInstance.getEntry("tv").getDouble(0.0) == 1.0);
+        }
+        return hits;
+    }
+
+    public double[] botPose() {
+        double[] botPose = null;
+        //SmartDashboard.putBoolean("Limelight Inititialized", isInitialized());
+        if (isInitialized()) {
+            botPose = botpose.getDoubleArray(new double[7]);
+        }
+        return botPose;
+    }
+
+    public double tl() {
+        double tL = 0.0;
+        if (isInitialized()) {
+            tL = tl.getDouble(0.0);
+        }
+        return tL;
+    }
+
+    public double cl() {
+        double cL = 0.0;
+        if (isInitialized()) {
+            cL = cl.getDouble(0.0);
+        }
+        return cL;
+    }
+
+    public double targetDist() {
+        double[] targetPose = null;
+        if (isInitialized()) {
+            targetPose = targetpose.getDoubleArray(new double[3]);
+        }
+        Translation3d dist = new Translation3d(targetPose[0], targetPose[1], targetPose[2]);
+        return dist.getDistance(new Translation3d());
+    }
+
+
+    public double targetArea() {
+        double dArea = 0.0;
+        if (isInitialized()) {
+            dArea = taEntry.getDouble(0.0);
+        }
+        return dArea;
+    }
+
+    public double tv() {
+        double tv = 0.0;
+        if (isInitialized()) {
+            tv = tTarget.getDouble(0.0);
+        }
+        return tv;
+    }
+
+
     @Override
     public void periodic() {
+        tableInstance = NetworkTableInstance.getDefault().getTable("limelight");
+        try {
+            tTarget = tableInstance.getEntry("tv");
+            txEntry = tableInstance.getEntry("tx");
+            tyEntry = tableInstance.getEntry("ty");
+            taEntry = tableInstance.getEntry("ta");
+            if (this.alliance == Alliance.Blue) {
+                botpose = tableInstance.getEntry("botpose_wpiblue");
+            } else {
+                botpose = tableInstance.getEntry("botpose_wpired");
+            }
+            targetpose = tableInstance.getEntry("targetpose_robotspace");
+            tl = tableInstance.getEntry("tl");
+            cl = tableInstance.getEntry("cl");
+            initialized = true;
+        } catch (Exception e) {
+            //SmartDashboard.putBoolean("couldn't get nt entries", true);
+        }
+
+
         double[] pose = tableInstance.getEntry("camerapose_targetspace").getDoubleArray(new double[6]);
 
 
-        tableInstance = NetworkTableInstance.getDefault().getTable("limelight");
     }
 }
