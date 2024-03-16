1fcd97322063be7b151c14646fc279ffab34afb5
Krish Parmar <krish2008.parmar@gmail.com>
2024-03-15 20:21:42 -0400

    auto work

--- src/main/java/frc/robot/commands/auto/AutoCommandFactory.java
+++ src/main/java/frc/robot/commands/auto/AutoCommandFactory.java
@@ -95,10 +95,13 @@ public class AutoCommandFactory {
             openLoop ? 0 : Swerve.rotPidKd
         );
 
+        System.out.println("X P: " + xController.getP());
+
 
         return Commands.sequence(
             Commands.print("Start X PID error: " + xController.getPositionError()),
             new ParallelRaceGroup(
+
                 Choreo.choreoSwerveCommand(
                     trajectory,
                     drivetrain::getPose,
@@ -111,7 +114,14 @@ public class AutoCommandFactory {
                 ),
                 Commands.repeatingSequence(new InstantCommand(() -> System.out.println("X SetPoint: " + xController.getSetpoint())),
                     new InstantCommand(() -> System.out.println("Translation error: " + +(xController.getSetpoint() - drivetrain.getPose().getX()))),
-                    new InstantCommand(() -> System.out.println("X POse: " + drivetrain.getPose().getX()))))
+                    new InstantCommand(() -> System.out.println("X POse: " + drivetrain.getPose().getX())),
+                    new InstantCommand(()-> Logger.recordOutput("X setpoint", xController.getSetpoint())),
+                    new InstantCommand(()-> Logger.recordOutput("Translation X", drivetrain.getPose().getX())),
+                    new InstantCommand(()-> Logger.recordOutput("Y setpoint", yController.getSetpoint())),
+                    new InstantCommand(()-> Logger.recordOutput("Translation Y", drivetrain.getPose().getY())),
+                    new InstantCommand(()-> Logger.recordOutput("Rot setpoint", rotationController.getSetpoint())),
+                    new InstantCommand(()-> Logger.recordOutput("Translation Rot", drivetrain.getPose().getRotation().getDegrees()))
+                    ))
         );
     }
 
--- src/main/java/frc/robot/subsystems/drive/YagslDriveTrain.java
+++ src/main/java/frc/robot/subsystems/drive/YagslDriveTrain.java
@@ -49,6 +49,7 @@ public class YagslDriveTrain extends DrivetrainBase {
         File directory = new File(Filesystem.getDeployDirectory(), Swerve.configDirectory);
         swerveDrive = new SwerveParser(directory).createSwerveDrive(m_maxVelocityMetersPerSecond);
         swerveDrive.setHeadingCorrection(false);
+//        swerveDrive.setHeadingCorrection(true);
         swerveModules = swerveDrive.getModules();
 
         // Disables cosine compensation for simulations since it causes discrepancies not seen in real life.
@@ -163,10 +164,8 @@ public class YagslDriveTrain extends DrivetrainBase {
     );
 
     public void setSysIdVoltage(double v) {
-        for (SwerveModule sm : swerveModules) {
-            sm.setAngle(0);
-            sm.getDriveMotor().setVoltage(v);
-        }
+            this.setDriveSpeedScale(1.0);
+            this.percentOutputDrive(new ChassisSpeeds(v/12.0, 0, 0), false);
     }
 
     public double getSysIdVoltage() {
@@ -182,7 +181,7 @@ public class YagslDriveTrain extends DrivetrainBase {
 
     @Override
     public Command getSysIdCommand() {
-        return SwerveDriveTest.generateSysIdCommand(sysIdRoutine, 1, 2, 2);
+        return SwerveDriveTest.generateSysIdCommand(sysIdRoutine, 1, 4, 2);
 
     }
 
@@ -225,6 +224,8 @@ public class YagslDriveTrain extends DrivetrainBase {
         } else {
             swerveDrive.drive(m_chassisSpeeds);
         }
+        Logger.recordOutput("Drive Pose", swerveDrive.getPose());
+        Logger.recordOutput("Drive velocity", swerveDrive.getRobotVelocity());
         for (int i = 0; i < swerveModules.length; i++) {
             Logger.recordOutput("Position drive module " + i, swerveModules[i].getDriveMotor().getPosition());
             Logger.recordOutput("Velocity drive module " + i, swerveModules[i].getDriveMotor().getVelocity());
@@ -233,6 +234,8 @@ public class YagslDriveTrain extends DrivetrainBase {
             Logger.recordOutput("Velocity angle module " + i, swerveModules[i].getAngleMotor().getVelocity());
             Logger.recordOutput("Volts angle module " + i, swerveModules[i].getAngleMotor().getVoltage());
         }
+
+
 
     }
 }
